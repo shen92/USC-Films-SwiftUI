a78d878e3cf77080cf39a0ff400793f5b48d3aca
Yingjie Shen <shen92@wisc.edu>
2021-04-27 12:30:13 -0500

    context menu clip

--- USC Films/Components/PosterList.swift
+++ USC Films/Components/PosterList.swift
@@ -20,35 +20,71 @@ struct PosterList: View {
       ScrollView(.horizontal) {
         HStack(alignment: .top){
           if(data.count != 0){
-            ForEach(data) { item in
-              NavigationLink(
-                destination: DetailsView(id: item.id, mediaType: item.mediaType),
-                label: {
-                  VStack(alignment: .center){
-                    RemoteImage(url: item.posterPath)
-                      .aspectRatio(contentMode: .fit)
-                      .cornerRadius(10.0)
-                      .frame(width: 92);
-                    Text("\(item.name)")
-                      .font(.caption)
-                      .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                      .multilineTextAlignment(.center)
-                      .frame(width: 92)
-                      .foregroundColor(colorScheme == .dark ? .white: .black);
-                    Text("(\(item.date))")
-                      .font(.caption)
-                      .multilineTextAlignment(.center)
-                      .frame(width: 92)
-                      .foregroundColor(Color.gray);
-                  }
-                  .padding(.trailing)
-                })
+            //            DetailsView(id: item.id, mediaType: item.mediaType)
+            ForEach(self.data){ item in
+              VStack(alignment: .center){
+                RemoteImage(url: item.posterPath)
+                  .aspectRatio(contentMode: .fit)
+                  .cornerRadius(10.0)
+                  .frame(width: 92)
+                Text("\(item.name)")
+                  .font(.caption)
+                  .fontWeight(.bold)
+                  .multilineTextAlignment(.center)
+                  .frame(width: 92)
+                  .foregroundColor(colorScheme == .dark ? .white: .black);
+                Text("(\(item.date))")
+                  .font(.caption)
+                  .multilineTextAlignment(.center)
+                  .frame(width: 92)
+                  .foregroundColor(Color.gray);
+              }
+              .background(colorScheme == .dark ? Color.black: Color.white)
+              .clipShape(RoundedRectangle(cornerRadius: 10))
+              .contentShape(RoundedRectangle(cornerRadius: 10))
+              .contextMenu{
+                VStack {
+                  Button(
+                    action: {},
+                    label: {
+                      HStack{
+                        Text("Add to watchList")
+                        Image(systemName: "bookmark")
+                      }
+                    }
+                  )
+                  Button(
+                    action: {},
+                    label: {
+                      HStack{
+                        Text("Share on Facebook")
+                        Image(colorScheme == .dark ? "facebook-dark" : "facebook")
+                          .resizable()
+                          .aspectRatio(contentMode: .fit)
+                          .frame(width: 20, height: 20, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                      }
+                    }
+                  )
+                  Button(
+                    action: {},
+                    label: {
+                      HStack{
+                        Text("Share on Twitter")
+                        Image(colorScheme == .dark ? "twitter-dark" : "twitter")
+                          .resizable()
+                          .aspectRatio(contentMode: .fit)
+                          .frame(width: 20, height: 20, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                      }
+                    }
+                  )
+                }
+              }
+              .padding(.trailing)
             }
           }
         }
       }
+      .frame(maxWidth: .infinity, alignment: .topLeading)
     }
-    .frame(maxWidth: .infinity, alignment: .topLeading)
   }
 }
-
