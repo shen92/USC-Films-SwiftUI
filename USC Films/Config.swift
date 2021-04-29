5708f002109729b35c18746c4c29b1913cd6c26e
Yingjie Shen <shen92@wisc.edu>
2021-04-28 23:25:39 -0500

    Allow insecure requests

--- USC Films/Config.swift
+++ USC Films/Config.swift
@@ -9,6 +9,13 @@ import Foundation
 
 enum Config: String {
   
-  case BASE_URL = "https://mobile-shen92-uscfilms.wl.r.appspot.com"
+  /* Localhost */
+  //case BASE_URL = "http://localhost:8080"
+  
+  /* GCP */
+  //case BASE_URL= "https://mobile-shen92-uscfilms.wl.r.appspot.com"
+  
+  /* AWS */
+  case BASE_URL = "http://express-env.eba-nreuxcka.us-west-1.elasticbeanstalk.com"
   
 }
